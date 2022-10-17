cb332c4750813a0ea69a9a297d69a6f5b7fb8b65
Sollydiran <adediranadesola2001@gmail.com>
2022-10-17 15:11:05 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * Main - entry point
+ *
+ * Retutn: 0
+ */
+int main(void)
+{
+	puts("\ "Programing is line building a multilingual puzzle");
+	return (0);
+}
