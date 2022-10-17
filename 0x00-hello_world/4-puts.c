9442e45b80f1af051d5b5c11e31475bc3325ef19
Sollydiran <adediranadesola2001@gmail.com>
2022-10-17 15:31:23 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * Main - entry point
- *
+ * Description: we dey try use putschar
  * Retutn: 0
  */
 int main(void)
 {
-	puts("\ "Programing is line building a multilingual puzzle");
+	puts("\"Programing is line building a multilingual puzzle");
 	return (0);
 }
+r
