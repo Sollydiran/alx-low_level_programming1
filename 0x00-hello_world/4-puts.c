e3e1df5d2471531596c67c332f16797f74b4da4c
Sollydiran <adediranadesola2001@gmail.com>
2022-10-17 16:43:19 -0700

    hhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * Retutn: always 0
  */
 int main(void)
-{
-i	puts("\"Programing is like building a multilingual puzzle");
-	return (0);
+{	
+ 	puts("\"Programing is like building a multilingual puzzle");
+	return (0); 
 }
 
