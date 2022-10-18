0e191e202325e7af720aa37fc3a56de9d485a467
Sollydiran <adediranadesola2001@gmail.com>
2022-10-18 09:05:44 -0700

    hhh

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* main entry point
+ *
+ * Description: A printf statement not allowed
+ * 		not allowed to use puts
+ * 		must end line
+ * Return: Always: 0 (Sucess)
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	return(0)
+}
