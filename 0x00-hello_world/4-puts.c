97302ada3c4c85187832b93b446bbfa1161daad8
Sollydiran <adediranadesola2001@gmail.com>
2022-10-18 07:50:08 -0700

    hhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints a text
+ * Main: Prints a text
  *
- * Retutn: always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
-{	
- 	puts("\"Programing is like building a multilingual puzzle");
-	return (0); 
+{
+	put("\ "Programming is like building a multilingual puzzle");
+	return (0);
 }
-
